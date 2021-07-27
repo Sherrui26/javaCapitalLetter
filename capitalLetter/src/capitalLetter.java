5d86d857f8a6cc12c105b3c52e8f9b9eac4cb406
Sherrui <Sharulaiman98@gmail.com>
2021-07-27 13:39:27 +0800

    javaCapitalLetter
    imported from pc to github

---
+++ capitalLetter/src/capitalLetter.java
@@ -0,0 +1,13 @@
+
+public class capitalLetter {
+    public static void main(String[] args){
+        
+        String a = "this is a test";
+        
+        //System.out.println(a.substring(1));
+        System.out.println(a.length());
+        System.out.println(a.substring(0, 14).toUpperCase());
+        System.out.println(a.substring(0, 1).toUpperCase()+a.substring(1));
+        
+    }
+}
